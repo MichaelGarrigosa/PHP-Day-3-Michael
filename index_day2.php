5da5c92bdf3acd37ee22c15d3e77106e28436dae
MichaelGarrigosa <mgarrigosa90@gmail.com>
2022-07-13 15:42:46 +0200

    1542

--- index_day2.php
+++ index_day2.php
@@ -1,7 +1,13 @@
 <?php
+$num =2
+function local (){
+    global $sum;
+    echo sum;
 
+}
+local()
 
-$age = 32 ;
-$age -= 1; $age = $age -1;
-echo $age;
+//$age = 32 ;
+//$age -= 1; $age = $age -1;
+//echo $age;
 //echo 'I am $name $surname and I am $age year old';
